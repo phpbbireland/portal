1dec75949646b0419bfd8da1919784e0d83f50a2
Prosk8er <prosk8er@gotskillslounge.com>
2022-01-09 22:16:02 -0500

    fix login issue with user information block

--- blocks/block_user_information.php
+++ blocks/block_user_information.php
@@ -40,6 +40,9 @@ $block_cache_time = (isset($block_cache_time) ? $block_cache_time : $k_config['k
 
 get_user_rank($this->user->data['user_rank'], (($this->user->data['user_id'] == ANONYMOUS) ? false : $this->user->data['user_posts']), $rank_title, $rank_img, $rank_img_src);
 
+// Last visit date/time
+$s_last_visit = ($user->data['user_id'] != ANONYMOUS) ? $user->format_date($user->data['session_last_visit']) : '';
+
 // Generate logged in/logged out status
 if ($this->user->data['user_id'] != ANONYMOUS)
 {
@@ -52,12 +55,23 @@ else
 	$l_login_logout = $this->user->lang['LOGIN'];
 }
 
+$s_login_redirect = build_hidden_fields(array('redirect' => append_sid("{$this->phpbb_root_path}portal.{$this->php_ext}")));
+
+// Add form token for login box, in case page is presenting a login form.
+add_form_key('login', '_LOGIN');
+
 $this->template->assign_vars([
-	'AVATAR'          => phpbb_get_user_avatar($this->user->data, $user->lang['USER_AVATAR'], false),
-	'WELCOME_SITE'    => sprintf($this->user->lang['WELCOME_SITE'], $this->config['sitename']),
-	'USR_RANK_TITLE'  => $rank_title,
-	'USR_RANK_IMG'    => $rank_img,
-	'U_LOGIN_LOGOUT'  => $u_login_logout,
-	'L_LOGIN_LOGOUT'  => $l_login_logout,
-	'S_LOGIN_ACTION'  => append_sid("{$this->phpbb_root_path}ucp.{$this->php_ext}", 'mode=login'),
+	'AVATAR'		=> phpbb_get_user_avatar($this->user->data, $user->lang['USER_AVATAR'], false),
+	'WELCOME_SITE'		=> sprintf($this->user->lang['WELCOME_SITE'], $this->config['sitename']),
+	'LAST_VISIT_DATE'	=> sprintf($user->lang['YOU_LAST_VISIT'], $s_last_visit),
+	'CURRENT_TIME'		=> sprintf($user->lang['CURRENT_TIME'], $user->format_date(time(), false, true)),
+	'USR_RANK_TITLE'	=> $rank_title,
+	'USR_RANK_IMG'		=> $rank_img,
+	'U_LOGIN_LOGOUT'	=> $u_login_logout,
+	'L_LOGIN_LOGOUT'	=> $l_login_logout,
+	'S_LOGIN_ACTION'	=> append_sid("{$this->phpbb_root_path}ucp.{$this->php_ext}", 'mode=login'),
+	'S_LOGIN_REDIRECT'	=> $s_login_redirect,
+	'U_SEARCH_NEW'		=> append_sid("{$this->phpbb_root_path}search.{$this->php_ext}", 'search_id=newposts'),
+	'U_SEARCH_SELF'		=> append_sid("{$this->phpbb_root_path}search.{$this->php_ext}", 'search_id=egosearch'),
+	'U_SEARCH_UNREAD'	=> append_sid("{$this->phpbb_root_path}search.{$this->php_ext}", 'search_id=unreadposts'),
 ]);
